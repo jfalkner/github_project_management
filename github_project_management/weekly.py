5410857839d6c936f458fc1f4ab8056bd30d8c3c
Jayson Falkner <jayson@counsyl.com>
2015-04-03 14:35:54 -0700

    Weekly now auto-posts, labels, and shows exec summary

--- github_project_management/weekly.py
+++ github_project_management/weekly.py
@@ -1,5 +1,7 @@
 from collections import Counter
 from datetime import datetime
+from datetime import timedelta
+from datetime import date
 import pytz
 
 import github3
@@ -13,7 +15,13 @@ def weekly(
     gh_api_url,
     repos,
     labels,
+    group_name,
+    # Defaults: only display the GH issue and format dates in ISO style.
+    test=True,
     date_format=lambda x: x.strftime('%Y-%m-%d')):
+
+    # Add the weekly label for the Weeklies.
+    weekly_labels = labels + ['Weekly']
 
     # Login to the GH enterprise server.
     gh = github3.github.GitHubEnterprise(gh_api_url)
@@ -22,10 +30,13 @@ def weekly(
 
     # Track all issues in the timeframe.
     tz = pytz.timezone('US/Pacific')
-    start_date = tz.localize(datetime.strptime('2015-03-30', '%Y-%m-%d'))
-    end_date = tz.localize(datetime.strptime('2015-04-05', '%Y-%m-%d'))
-#    start_date = tz.localize(datetime.strptime('2015-03-23', '%Y-%m-%d'))
-#    end_date = tz.localize(datetime.strptime('2015-03-29', '%Y-%m-%d'))
+    today = tz.localize(datetime.today())
+    today -= timedelta(seconds=(today.hour * 60 * 60 + today.minute * 60 + today.second), microseconds=today.microsecond)
+    current_week_monday = today - timedelta(days=today.weekday())
+    current_week_sunday = current_week_monday + timedelta(days=6)
+    last_week_monday = current_week_monday - timedelta(days=7)
+    last_week_sunday = current_week_sunday - timedelta(days=7)
+
 
     milestones = []
 
@@ -44,9 +55,9 @@ def weekly(
                     milestones.append((None, None))
 
             # Skip if the create or close date doesn't make sense given the time range.
-            if issue.closed_at and issue.closed_at < start_date:
+            if issue.closed_at and issue.closed_at < current_week_monday:
                 continue
-            if issue.created_at > end_date:
+            if issue.created_at > current_week_sunday:
                 continue
 
             row = {}
@@ -69,13 +80,13 @@ def weekly(
             comments = []
             for com in issue.iter_comments():
                 # skip comments note in the time range of ineterest.
-                if com.updated_at < start_date:
+                if com.updated_at < current_week_monday:
                     continue
-                if com.created_at > end_date:
+                if com.created_at > current_week_sunday:
                     continue
 
                 # keeping based on created_at has the downside that updates will mutate the data.
-                if com.created_at > start_date and com.created_at <= end_date:
+                if com.created_at > current_week_monday and com.created_at <= current_week_sunday:
                     comments.append({
                         'body': com.body,
                         'created': com.created_at,
@@ -84,111 +95,134 @@ def weekly(
             row['comments'] = comments
 
             # don't show this issues if there were no comments.
-            if len(comments) > 0 or (issue.updated_at <= end_date and issue.updated_at >= start_date):
+            if len(comments) > 0 or (issue.created_at <= current_week_sunday and issue.created_at >= current_week_monday):
                 # display the results.
                 rows.append(row)
 
-    # Make the weekly title. Use it for a unique check.
+    # make the weekly title. use it for a unique check.
     def suffix(d):
         return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
 
     def custom_strftime(format, t):
-        return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+        return t.strftime(format).replace('{s}', str(t.day) + suffix(t.day))
 
-    title = "%s Weekly %s - %s" % (
-                   'Curation',
-                   custom_strftime('%b {S}', start_date),
-                   custom_strftime('%b {S}', end_date))
+    def make_title(name, start_date, end_date):
+        return "%s Weekly %s - %s" % (
+                   name,
+                   custom_strftime('%b {s}', start_date),
+                   custom_strftime('%b {s}', end_date))
 
-    print title
-    # Nate the point of this issue.
-    print '@dev/curators @dev/pgms @dev/genomics\n'
 
-    print 'This is the Curation weekly GitHub issue. The purpose of it is to summarize needing work and related discussions. GitHub is used because it is the primary tool Counsyl engineers use to track work and report progress.\n'
+    current_week_title = make_title(group_name, current_week_monday, current_week_sunday)
+    last_week_title = make_title(group_name, last_week_monday, last_week_sunday)
 
-    print 'You can find out more about [Curation here](https://docs.google.com/a/counsyl.com/document/d/1Sv-8omVeYK5F5mywlrkMbtCqzITf4XSvoK-bEnidtA4/edit?usp=sharing). More information about the weekly, monthly and project management process in [here](https://docs.google.com/a/counsyl.com/document/d/1k2Qrqk-hPJKmLh_JZsiYFr7lh6s3Qc9lTABTxiUvARI/edit?usp=sharing).\n'
+    # Search for a GH issue that already has this title. If exists, reuse it.
+    print "Searching for this and last week's issues"
+    repo = gh.repository(repo_user, repo_name)
+    weekly_issue = None
+    last_weekly_issue = None
+    for issue in repo.iter_issues(state='all', labels=','.join(labels)):
+        if issue.title == current_week_title:
+            weekly_issue = issue
+        if issue.title == last_week_title:
+            last_weekly_issue = issue
+    # If weekly issue doesn't exist, make it.
+    print "  Found this week's issue: %s" % (weekly_issue)
+    print "  Found last week's issue: %s" % (last_weekly_issue)
+    print current_week_title
+
+    # Build up the body of the current Weekly GH issue.
+    body = ''
+    body += '@dev/curators @dev/pgms @dev/genomics\n\n'
+
+    body += 'This is the curation weekly github issue. The purpose of it is to summarize needing work and related discussions. github is used because it is the primary tool counsyl engineers use to track work and report progress.\n\n'
+
+    body += 'You can find out more about [curation here](https://docs.google.com/a/counsyl.com/document/d/1sv-8omveyk5f5mywlrkmbtcqzitf4xsvok-benidta4/edit?usp=sharing). More information about the weekly, monthly and project management process in [here](https://docs.google.com/a/counsyl.com/document/d/1k2qrqk-hpjkmlh_jzsiyfr7lh6s3qc9ltabtxiuvari/edit?usp=sharing).\n\n'
+
+    # First show the executive summary.
+    body += '\n### Executive Summary\n\n'
+    executive_summary_comments = []
+    if weekly_issue:
+        for com in weekly_issue.iter_comments():
+            if com.body and com.body.startswith('Executive Summary:'):
+                executive_summary_comments.append(com.body[len('Executive Summary:'):])
+    if not executive_summary_comments:
+        body += '- No executive summary comments.\n'
+    else:
+        for esc in executive_summary_comments:
+            body += '- %s\n' % esc
+
 
     rows = sorted(rows, key=lambda x: len(x['comments']), reverse=True)
-    print '### Active this week\n'
-    open_issues = [row for row in rows if issue['state'] == 'open']
-    for issue in open_issues:
-        print '- %d comment%s: [%s](%s)' % (
-            len(issue['comments']),
-            's' if len(issue['comments']) > 1 else '',
-            issue[GPMC.TITLE],
-            issue[GPMC.URL])
-    if not open_issues:
-        print "- No comments in issues labeled 'curation' this week"
 
-    print '\n### Closed this week\n'
-    closed_issues = [row for row in rows if issue['state'] != 'open']
+    # Show all active GH issues as a list sorted by most comments.
+    body += '\n### Active this week\n\n'
+    open_issues = [row for row in rows if row['state'] == 'open']
+    for issue in open_issues:
+        num_comments = len(issue['comments'])
+        if num_comments:
+            body += '- %d comment%s: [%s](%s)\n' % (
+                num_comments,
+                's' if num_comments > 1 else '',
+                issue[GPMC.TITLE],
+                issue[GPMC.URL])
+        else:
+            body += '- New issue: [%s](%s)\n' % (
+                issue[GPMC.TITLE],
+                issue[GPMC.URL])
+    if not open_issues:
+        body += "- No comments in issues labeled 'curation' this week\n"
+
+    # Show all closed GH issues as a list sorted by most comments.
+    body += '\n### Closed this week\n\n'
+    closed_issues = [row for row in rows if row['state'] != 'open']
     for issue in closed_issues:
-        print '- %d comment%s: [%s](%s)' % (
+        body += '- %d comment%s: [%s](%s)\n' % (
             len(issue['comments']),
             's' if len(issue['comments']) > 1 else '',
             issue[GPMC.TITLE],
             issue[GPMC.URL])
     if not closed_issues:
-        print '- No issues closed this week.'
+        body += '- No issues closed this week.\n'
 
-    print '\n### Milestones'
-    print '\nThese are groups of tickets related to a specific project. Misc inactive tickets are binned in the "Curation Backlog" milestone. This list is intended to help summarize the [full set of tickets open for Curation](https://github.counsyl.com/dev/website/labels/curation).\n'
+    body += '\n### Milestones\n'
+    body += '\nThese are groups of tickets related to a specific project. misc inactive tickets are binned in the "curation backlog" milestone. this list is intended to help summarize the [full set of tickets open for curation](https://github.counsyl.com/dev/website/labels/curation).\n\n'
     for (title, url), count in Counter(milestones).most_common():
         if title:
-            print '- %d open issue%s: [%s](%s)' % (count, 's' if count > 1 else '', title, url)
+            body += '- %d open issue%s: [%s](%s)\n' % (count, 's' if count > 1 else '', title, url)
         else:
-            print '- %d open issue%s assigned to a milestone' % (count, "s aren't" if count > 1 else " isn't")
+            body += '- %d open issue%s assigned to a milestone\n' % (count, "s aren't" if count > 1 else " isn't")
 
-
-#    # Flatten out all comments.
-#    for issue in rows:
-#        print '-', issue['state'], issue[GPMC.TITLE]
-#        comments = issue['comments']
-#        sorted(comments, key=lambda x: x['created'])
-#        for comment in comments:
-#            print '  -', comment['created'], comment['user'], comment['body']
-
-    import ipdb; ipdb.set_trace()
-
-    # Flatten and render last weeks remaining issues.
-    open_issues = []
-    for uoi in all_open_issues.iterkeys():
-        open_issues += list(all_open_issues[uoi])
-    all_issues = issues_to_markdown(open_issues)
-
-    body = self.body.encode('ascii', 'ignore').format(
-        active_issues=issues_to_markdown(active_issues).encode('ascii', 'ignore'),
-        closed_issues=closed_issues.encode('ascii', 'ignore'))
-
-
+    # Always run in test mode by default.
     if test:
         print('### Test Mode. Not posting to GH ###')
-        print(title)
+        print(current_week_title)
         print(body)
         return
 
-    # Search for a GH issue that already has this title. If exists, reuse it.
-    repo = gh.repository(self.weekly_gh_user, self.weekly_gh_repo)
-    weekly_issue = None
-    for issue in repo.iter_issues():
-        if issue.title == title:
-            weekly_issue = issue
-    # If weekly issue doesn't exist, make it.
+
     if not weekly_issue:
+        print 'Making new issue'
         weekly_issue = repo.create_issue(
-            title,
+            current_week_title,
             body=body,
-            assignee=self.weekly_user,
-            labels=self.weekly_labels)
+            assignee=gh_user,
+            labels=weekly_labels)
         if not weekly_issue:
             raise ValueException("Can't find or create weekly issue.")
         else:
-            logger.info('Lazy made GH issue #%d as weekly.' % weekly_issue.number)
+            print 'Lazy made GH issue #%d as weekly.' % weekly_issue.number
     # If the issue exists, update it.
     else:
         if weekly_issue.is_closed():
             weekly_issue.reopen()
         weekly_issue.edit(
-            title,
-            body)
-        logger.info('Updated: (%s, %s) #%d' % (user, repo, weekly_issue.number))
+            current_week_title,
+            body,
+            assignee=gh_user,
+            labels=weekly_labels)
+        print 'Updated: (%s, %s) #%d' % (gh_user, repo, weekly_issue.number)
+    # Make sure that last week's issue is closed.
+    if last_weekly_issue and not last_weekly_issue.is_closed():
+        last_weekly_issue.close()
+        print 'Closed old weekly: (%s, %s) #%d' % (gh_user, repo, last_weekly_issue.number)
