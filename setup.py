55c8d10bbf3aee07eee62579908a31a2f9059e13
Jayson Falkner <jayson@counsyl.com>
2015-04-03 16:59:52 -0700

    Bumped version. Weekly is now working.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='github_project_management',
-    version='0.0.1',
+    version='0.0.2',
     author='Jayson Falkner',
     author_email='jfalkner@gmail.com',
     maintainer='Jayson Falkner',
